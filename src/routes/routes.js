de0b0b3de61f2e9342ca222a5596ebd1f30f02df
caren <carenoliveirasilvapontes@gmail.com>
2022-12-14 21:36:38 -0500

    requisitos finais

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,67 @@
 const express = require('express');
-const { readfile, generateId } = require('../utils/utils');
+const { readfile, generateId, Writefile } = require('../utils/utils');
 const validateEmail = require('../middleares/validateEmail');
 const validatePassword = require('../middleares/validatePassword');
+const validateAutorization = require('../middleares/validateAutorization');
+const validateName = require('../middleares/validateName');
+const validateAge = require('../middleares/validateAge');
+const validateTalk = require('../middleares/validateTalk');
+const validateWatcheAt = require('../middleares/validateWatcheAt');
+const validateRate = require('../middleares/validateRate');
+const continuationValidateRate = require('../middleares/validateRateContinue');
 
 const route = express.Router();
-
-route.post('/login', validateEmail, validatePassword, (req, res) => {
+route.put('/talker/:id', validateAutorization,
+validateName, validateAge, validateTalk,
+validateWatcheAt, continuationValidateRate,
+validateRate, async (req, res) => {
+const { id } = req.params;
+const talkers = await readfile();
+const talkerI = talkers.indexOf((t) => t.id === Number(id));
+talkers.splice(talkerI, 1);
+const talker = { id: talkers.length + 1, ...req.body };
+talkers.push(talker);
+await Writefile(JSON.stringify(talkers, null, 2));
+return res.status(200).json(talker);
+});
+route.post('/login', validateEmail,
+ validatePassword, (req, res) => {
   const token = generateId();
   res.status(200).json({ token: `${token}` });
+});
+
+route.post('/talker', 
+validateAutorization, 
+validateName, validateAge,
+ validateTalk, validateWatcheAt, 
+ continuationValidateRate,
+  validateRate, async (req, res) => {
+  const talkers = await readfile();
+  const id = talkers.length + 1;
+   const talker = { id, ...req.body };
+   talkers.push(talker);
+   await Writefile(JSON.stringify(talkers, null, 2));
+    return res.status(201).json(talker);
+});
+route.delete('/talker/:id', validateAutorization, async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readfile();
+  const exist = talkers.indexOf((t) => t.id === Number(id));
+  talkers.splice(exist, 1);
+  await Writefile(JSON.stringify(talkers, null, 2));
+  return res.status(204).end();
+});
+route.get('/talker/search', validateAutorization, async (req, res) => {
+  const { q } = req.query;
+  const talkers = await readfile();
+  const talker = talkers.filter((e) => e.name.includes(q));
+  if (!q) {
+    return res.status(200).json(talkers);
+  }
+  if (!talker) {
+    return res.status(200).json([]);
+  }
+  return res.status(200).json(talker);
 });
 
 route.get('/talker', async (req, res) => {
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,13 +1,16 @@
-const { readFile } = require('fs').promises;
+const fs = require('fs').promises;
 const path = require('path');
 const crypto = require('crypto');
 
 const usePath = path.resolve(__dirname, '..', 'talker.json');
 
 const readfile = async () => {
-  const response = await readFile(usePath, 'utf8');
+  const response = await fs.readFile(usePath, 'utf8');
   const talkers = JSON.parse(response);
   return talkers;
+};
+const Writefile = async (talkers) => {
+  await fs.writeFile('src/talker.json', talkers);
 };
 
 const generateId = () => crypto.randomBytes(8).toString('hex');
@@ -15,5 +18,6 @@ const generateId = () => crypto.randomBytes(8).toString('hex');
 module.exports = {
   readfile,
   generateId,
+  Writefile,
 
 };
